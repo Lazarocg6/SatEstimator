bf8f03762e1f4d199bc5e78dda1589bd26ec7304
Lazaro <lazarocg6@gmail.com>
2025-03-18 00:47:31 +0100

    Funciona -> Incluidos filtro en Acimut y elevacion Cambiado -> graphs.m cambiado de funcinamiento matricial a vectorial para poder tener tsince de distinta longitud Corregido -> - Por hacer -> Revisar, optimizar, añadir ecuacion radar

--- main.m
+++ main.m
@@ -3,20 +3,22 @@ clear
 close all
 %Instante duracion precision
 inst = datetime('now', 'TimeZone', 'UTC'); %Time origin
-% inst = datetime('8-Mar 23:30:17','InputFormat','dd-MMM HH:mm:ss',TimeZone='Local');
+% inst = datetime('13-Mar 11:11:17','InputFormat','dd-MMM HH:mm:ss',TimeZone='Local');
 duracion = 60; % In minutes, 'duracion' before and after the current time
-precision = 60 / 60; % Precision in minutes
+precision = 30 / 60; % Precision in minutes
 
 % Satellites
 ISS = struct('Name', 'ISS', 'NORAD', 25544);
 Starlink = struct('Name', 'Starlink', 'NORAD', 44737);
-OneWeb = struct('Name', 'OneWeb', 'NORAD', 48968);
+OneWeb = struct('Name', 'OneWeb', 'NORAD', 44057);
 
 sats = [ISS OneWeb];
+
+AzElFilter = true;
 
 % Coordinates
 
 ETSIT = [40.45206046037957, -3.726407299669201, 670];
 Vill = [40.6223011985758, -4.010124224894723, 930];
 
-graphs(sats,inst,duracion,precision,Vill)
+graphs(sats,inst,duracion,precision,Vill,AzElFilter)
